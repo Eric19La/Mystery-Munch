5ecd69988b54ca2129e424eca2800f1450a486d2
Eric19La <Eric19La@github.com>
2025-03-21 15:44:28 -0700

    Working on Settings Screen

--- mobile_app/lib/screens/settings_screen.dart
+++ mobile_app/lib/screens/settings_screen.dart
@@ -8,15 +8,40 @@ class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Settings")),
-      body: Center(
-        child: Text("This is the settings screen"),
+      appBar: AppBar(
+        title: Text("Settings"),
+        // elevation: 0,
+        // toolbarHeight: 40,
+      ),
+      body: ListView(
+        children: const [
+          SectionHeader(title: "Profile Info"),
+          ListTile(leading: Icon(Icons.person), title: Text("Edit Profile")),
+          Divider(),
+        ],
       ),
 
       // Nav Bar
       bottomNavigationBar: BottomNavBar(
         selectedIndex: 2,
         onPageSelected: (index) => selectedPage(context, index),
+      ),
+    );
+  }
+}
+
+
+class SectionHeader extends StatelessWidget {
+  final String title;
+  const SectionHeader({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Text(
+        title,
+        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
       ),
     );
   }
